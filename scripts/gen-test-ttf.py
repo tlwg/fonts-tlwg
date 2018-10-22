af8c5a36bde386eca440d7f4b960226a6283718a
Theppitak Karoonboonyanan <theppitak@gmail.com>
2018-10-22 17:24:20 +0700

    Test TTF: Generate test TTFs from 'Quad' layer
    * scripts/Makefile.am, +scripts/gen-test-ttf.py:
      - Rewrite gen-test-ttf.pe in Python
    * configure.ac:
      - Check for "fontforge" Python module on --enable-testfont option
    * scripts/gen.mk.am:
      - Replace test TTF generation with Python script

---
+++ scripts/gen-test-ttf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import fontforge
+
+sfd_file = sys.argv[1]
+
+f = fontforge.open(sfd_file)
+
+# Prepend "TTF" prefix to font/family name
+f.fontname = "TTF" + f.fontname
+f.familyname = "TTF" + f.familyname
+f.fullname = "TTF" + f.fullname
+
+# Prepend "TTF" prefix to "Preferred Family"
+for name in f.sfnt_names:
+    if name[0] == "English (US)" and name[1] == "Preferred Family":
+        f.appendSFNTName(name[0], name[1], "TTF" + name[2])
+        break
+
+f.generate(f.fontname + ".ttf", layer="Quad")
